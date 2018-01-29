4ed48f3d1592e147fefecc01ab755b17761a98e1
CJordanGoodman <good.cjordan@gmail.com>
2018-01-28 17:07:53 -0800

    Create moderndaymondrian-1different.js

---
+++ moderndaymondrian-1different.js
@@ -0,0 +1,37 @@
+background("white");
+
+ellipse(width/2, height/2, width, height/2);
+line(width/2, height/4, width/2, 3 * height/4);
+line(24, height/8 * 3, width/2, height/8 * 3);
+line(width/3, 90, width/3, height/8 * 5);
+line(48, height/8*3, 48, 229);
+line(0, height/2, width/3, height/2);
+line(5, 150, 48, 150);
+line(48, height/8 * 5, 315, height/8* 5);
+line(width/12 * 5, 86, width/12 * 5, height/8 * 3);
+line(width/3 * 2, height/2, width/3 * 2, 250);
+line(width/2, height/2, 280, height/2);
+line(width/12 * 7, 86, width/12 * 7, height/8 * 5);
+line(width/2, 105, 280, 105);
+line(280, 105, 280, height/8 * 5);
+line(280, 140, 328, 140);
+
+
+
+colorMode(HSB);
+fill("red");
+rect(48, height/8*3, 65, 43);
+rect(width/2, 212, 56, 38);
+rect(width/12*7 + 28, 170, 54, 42);
+
+fill("yellow");
+rect(48, 170, 65, 42);
+rect(width/12*7, 170, 28, 42);
+
+fill("black");
+rect(113, height/8*3, 57, 85);
+rect(48, 111, 65, 16);
+
+
+fill("blue");
+rect(width/12*7, 105, 81, 65);
